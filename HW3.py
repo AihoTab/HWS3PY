e1ac3e006767e90da8a14ee381e898cd600cd7df
Jull <jullweas@gmail.com>
2022-11-13 07:54:50 +0300

    task5

--- HW3.py
+++ HW3.py
@@ -1,8 +1,24 @@
-# 4 Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# 5 Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# Fn = Fn+1 - Fn+2 | F-n = (-1)n+1Fn. || F 0 = 0, F 1 = 1, F n = F n-1 + F n-2, n ≥ 2
 
 num = int(input())
-dvoich = ''
-while num != 0:
-    dvoich += str(num % 2)
-    num //= 2
-print(dvoich[::-1])
+fib = [0, 1]
+for i in range(num-2):
+    to_the_end = fib[-1]+fib[-2]
+    to_the_start = (-1) ** (i+1)*to_the_end
+    fib.append(to_the_end)
+    fib.insert(0, to_the_start)
+print(*fib)
+
+
+# num = int(input())
+# fib = ''
+# f_1 = f_2 = 1
+# for i in range(2, num):
+#     f_1, f_2 = f_2, f_1 + f_2
+#     #fib.append(f_1)
+#     print(f_2, end=' ')
+# for i in range(num):
+#     f_1, f_2 = f_2, f_1 - f_2
+#     fib.insert(0, f_1)
+# print(fib)
