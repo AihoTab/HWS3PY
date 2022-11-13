d935158625e8261a69e297507f9e12031ae3f191
Jull <jullweas@gmail.com>
2022-11-13 06:48:10 +0300

    task4

--- HW3.py
+++ HW3.py
@@ -1,17 +1,8 @@
-# 3 Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+# 4 Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
-num = list(map(float, input().split()))
-n_num = [round(i % 1, 2) for i in num if i % 1 != 0]
-print(max(n_num) - min(n_num))
-
-
-# num = [float(i) for i in input().split()]
-# max_ost = num[0] - int(num[0])
-# min_ost = num[0] - int(num[0])
-# for i in num:
-#     num - int(num)
-#     if num > max_ost:
-#         max_ost = num
-#     elif num[i] < min_ost:
-#         min_ost = num
-# print(max_ost - min_ost)
+num = int(input())
+dvoich = ''
+while num != 0:
+    dvoich += str(num % 2)
+    num //= 2
+print(dvoich[::-1])
